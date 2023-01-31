d393985e4874ca42dcf9452f3a75b54c04e1a4fe
zoorya <80897736+zoorya@users.noreply.github.com>
2023-01-31 21:42:21 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { Button, Label } from 'semantic-ui-react';
+import "./Components/CounterSemanticUI.css"
+
+function Counter() {
+    const [count, setCount] = useState(0);    
+    const [buttonClass, setButtonClass] = useState("primary");
+
+    const DecrementFn = () =>{
+        if(count <= 0){
+            setCount(0)
+            setButtonClass("secondary")   
+        }
+        else{
+            setCount(count - 1)
+            setButtonClass("primary")  
+        }
+    }
+
+    // Increment Function
+    const IncrementFn = () =>{
+        setCount(count + 1)
+        setButtonClass("primary")  
+    }
+
+    let socialcount = 0;
+    const social = () =>{
+        
+        }
+        socialcount +=1;
+    }
+    console.clear()
+
+  return (
+    <div className='main'>
+        <h4>A Counter Application Using useState()</h4>
+        <div className='Card'>
+            <Label className='Count'size="huge">Count </Label>
+            <Label className='CountNumber'size="huge">{count}</Label>
+            <Button.Group>
+                <Button className="increment" onClick={IncrementFn}> Increment </Button>
+                <Button className={buttonClass} onClick={DecrementFn}>Decrement </Button>
+            </Button.Group>
+        </div>
+        <p onMouseOver={social} className='credit'>Made with <span> love </span></p>
+    </div>
+  );    
+}
+
+export default Counter;
